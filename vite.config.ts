d317dd72ade92672f1187907144312a43b285df8
Adam Patryk Dzikowski <adam.dzikowski95@gmail.com>
2026-07-15 12:21:40 +0200

    added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/1z10/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,        // expose on local network
